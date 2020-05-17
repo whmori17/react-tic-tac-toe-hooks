5f0ab91704074adb46273a402668d446c606faeb
Gabriele Morisco <gabrielemori@outlook.com>
2020-05-17 21:01:00 +0200

    stepNumber has its own state

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -7,14 +7,9 @@ import { GameInfo } from '@components/GameInfo';
 export interface BoardSquares {
   squares: Move;
 }
-export interface GameState {
-  stepNumber: number;
-}
 
 export const Game: React.FC = () => {
-  const [state, setState] = useState<GameState>({
-    stepNumber: 0,
-  });
+  const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState<BoardSquares[]>([
     {
@@ -23,14 +18,12 @@ export const Game: React.FC = () => {
   ]);
 
   const jumpTo = (step: number): void => {
-    setState({
-      stepNumber: step,
-    });
+    setStepNumber(step);
     setXIsNext(step % 2 === 0);
   };
 
   const handleClick = (i: number): void => {
-    const currentHistory = history.slice(0, state.stepNumber + 1);
+    const currentHistory = history.slice(0, stepNumber + 1);
     const currentMove = currentHistory[currentHistory.length - 1];
     const squares = currentMove.squares.slice();
 
@@ -44,12 +37,10 @@ export const Game: React.FC = () => {
       return [...prevHistory, { squares }];
     });
     setXIsNext(!xIsNext);
-    setState({
-      stepNumber: history.length,
-    });
+    setStepNumber(history.length);
   };
 
-  const currentMove = history[state.stepNumber];
+  const currentMove = history[stepNumber];
   const winner = GameDirector.calculateWinner(currentMove.squares);
   const status = winner ? 'Winner is: ' + winner : 'Next player: ' + (xIsNext ? 'X' : 'O');
   const moves = history.map((move, step) => {
