e7cfd9c0e726d0624c3bb6463dc46a51fbe1fc18
Gabriele Morisco <gabrielemori@outlook.com>
2020-05-17 18:15:21 +0200

    Refactoring board

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -8,7 +8,7 @@ export interface BoardProps {
 
 export const Board: React.FC<BoardProps> = ({ squares, onClick }) => {
   const renderSquare = (i: number) => {
-    return <Square value={squares[i]} onClick={() => onClick(i)} />;
+    return <Square value={squares[i]} onClick={() => onClick(i)} key={i} />;
   };
 
   const firstRow = [0, 1, 2];
