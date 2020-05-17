82da65705bbf84c62eca5c79f04ab349f78970db
Gabriele Morisco <gabrielemori@outlook.com>
2020-05-17 21:08:42 +0200

    Semplification of squares within history state

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -4,18 +4,10 @@ import { GameMoveProps, GameMove, Board } from '..';
 import { Move } from '@customTypes/Move';
 import { GameInfo } from '@components/GameInfo';
 
-export interface BoardSquares {
-  squares: Move;
-}
-
 export const Game: React.FC = () => {
   const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXIsNext] = useState(true);
-  const [history, setHistory] = useState<BoardSquares[]>([
-    {
-      squares: Array(9).fill(null),
-    },
-  ]);
+  const [history, setHistory] = useState<Move[]>([Array(9).fill(null)]);
 
   const jumpTo = (step: number): void => {
     setStepNumber(step);
@@ -24,8 +16,8 @@ export const Game: React.FC = () => {
 
   const handleClick = (i: number): void => {
     const currentHistory = history.slice(0, stepNumber + 1);
-    const currentMove = currentHistory[currentHistory.length - 1];
-    const squares = currentMove.squares.slice();
+    const moves = currentHistory[currentHistory.length - 1];
+    const squares = moves.slice();
 
     if (GameDirector.calculateWinner(squares) || squares[i]) {
       return;
@@ -34,23 +26,23 @@ export const Game: React.FC = () => {
     squares[i] = xIsNext ? 'X' : 'O';
 
     setHistory(prevHistory => {
-      return [...prevHistory, { squares }];
+      return [...prevHistory, squares];
     });
     setXIsNext(!xIsNext);
     setStepNumber(history.length);
   };
 
-  const currentMove = history[stepNumber];
-  const winner = GameDirector.calculateWinner(currentMove.squares);
+  const move = history[stepNumber];
+  const winner = GameDirector.calculateWinner(move);
   const status = winner ? 'Winner is: ' + winner : 'Next player: ' + (xIsNext ? 'X' : 'O');
   const moves = history.map((move, step) => {
-    const props: GameMoveProps = { step: step, onClick: step => jumpTo(step) };
+    const props: GameMoveProps = { step, onClick: step => jumpTo(step) };
     return <GameMove {...props} key={step} />;
   });
 
   return (
     <div className="game">
-      <Board squares={currentMove.squares} onClick={(i: number) => handleClick(i)} />
+      <Board squares={move} onClick={(i: number) => handleClick(i)} />
       <GameInfo {...{ status, moves }} />
     </div>
   );
