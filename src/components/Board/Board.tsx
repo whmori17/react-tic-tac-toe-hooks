83f815c13cc2fb177dc4a026813ff10dfa1d65ea
Gabriele Morisco <gabrielemori@outlook.com>
2020-05-17 19:39:23 +0200

    Refactoring board

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -11,15 +11,18 @@ export const Board: React.FC<BoardProps> = ({ squares, onClick }) => {
   const firstRow = [0, 1, 2];
   const secondRow = [3, 4, 5];
   const thirdRow = [6, 7, 8];
+  const squaresMatrix = [firstRow, secondRow, thirdRow];
   const renderSquare = (i: number) => {
     return <Square value={squares[i]} onClick={() => onClick(i)} key={i} />;
   };
 
   return (
     <div className="game-board">
-      <div className="board-row">{firstRow.map(i => renderSquare(i))}</div>
-      <div className="board-row">{secondRow.map(i => renderSquare(i))}</div>
-      <div className="board-row">{thirdRow.map(i => renderSquare(i))}</div>
+      {squaresMatrix.map((row, index) => (
+        <div className="board-row" key={index}>
+          {row.map(i => renderSquare(i))}
+        </div>
+      ))}
     </div>
   );
 };
